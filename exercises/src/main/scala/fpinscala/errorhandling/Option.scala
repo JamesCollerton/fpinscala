bb33f506577f405c53797db85cb9535adff6e38b
James Collerton <James.Collerton@kobaltmusic.com>
2019-02-22 11:02:32 +0000

    Finished errors and exception handling

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -70,5 +70,8 @@ object Option {
     case h :: t => h.flatMap(x => sequence(t).map(newT => x :: newT)).orElse(None)
   }
 
-  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = ???
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = a match {
+    case Nil => Some(Nil)
+    case h :: t =>  map2(f(h), traverse(t)(f))(_ :: _)
+  }
 }
