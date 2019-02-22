8e79c8705fe73c4438ef893ac08c4efd519c0d40
James Collerton <James.Collerton@kobaltmusic.com>
2019-02-22 17:39:03 +0000

    Can probably consider the lazy chapter done@

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -1,15 +1,16 @@
 package fpinscala.laziness
 
-import Stream._
+import fpinscala.laziness.Stream._
+
 trait Stream[+A] {
 
   def foldRight[B](z: => B)(f: (A, => B) => B): B = // The arrow `=>` in front of the argument type `B` means that the function `f` takes its second argument by name and may choose not to evaluate it.
     this match {
-      case Cons(h,t) => f(h(), t().foldRight(z)(f)) // If `f` doesn't evaluate its second argument, the recursion never occurs.
+      case Cons(h, t) => f(h(), t().foldRight(z)(f)) // If `f` doesn't evaluate its second argument, the recursion never occurs.
       case _ => z
     }
 
-  def exists(p: A => Boolean): Boolean = 
+  def exists(p: A => Boolean): Boolean =
     foldRight(false)((a, b) => p(a) || b) // Here `b` is the unevaluated recursive step that folds the tail of the stream. If `p(a)` returns `true`, `b` will never be evaluated and the computation terminates early.
 
   @annotation.tailrec
@@ -22,29 +23,39 @@ trait Stream[+A] {
 
   def take(n: Int): Stream[A] = this match {
     case Empty => Empty
-    case Cons(h, t) => if(n > 0) cons(h(), t().take(n - 1)) else Empty
+    case Cons(h, t) => if (n > 0) cons(h(), t().take(n - 1)) else Empty
   }
 
   def drop(n: Int): Stream[A] = this match {
     case Empty => Empty
-    case Cons(h, t) => if(n <= 0) cons(h(), t().drop(n)) else t().drop(n - 1)
+    case Cons(h, t) => if (n <= 0) cons(h(), t().drop(n)) else t().drop(n - 1)
   }
 
-  def takeWhile(p: A => Boolean): Stream[A] = this match {
-    case Empty => Empty
-    case Cons(h, t) => if(!p(h())) cons(h(), t().takeWhile(p)) else Empty
-  }
+  def takeWhile(p: A => Boolean): Stream[A] =
+    foldRight(empty[A])((a, b) => if (p(a)) cons(a, b.takeWhile(p)) else empty)
 
-  def forAll(p: A => Boolean): Boolean = ???
+  def forAll(p: A => Boolean): Boolean =
+    foldRight(true)((a, b) => p(a) && b)
 
   def headOption: Option[A] = ???
 
   // 5.7 map, filter, append, flatmap using foldRight. Part of the exercise is
   // writing your own function signatures.
 
+  def map[B](f: A => B): Stream[B] = foldRight(empty[B])((a, b) => cons(f(a), b))
+
+  def filter(p: A => Boolean): Stream[A] = foldRight(empty[A])((a, b) => if (p(a)) cons(a, b) else b)
+
+  def append[B >: A](s: Stream[B]): Stream[B] = foldRight(s)((a, b) => cons(a, b))
+
+  def flatMap[B](f: A => Stream[B]): Stream[B] = foldRight(empty[B])((a, b) => f(a).append(b))
+
   def startsWith[B](s: Stream[B]): Boolean = ???
+
 }
+
 case object Empty extends Stream[Nothing]
+
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
 
 object Stream {
@@ -57,11 +68,32 @@ object Stream {
   def empty[A]: Stream[A] = Empty
 
   def apply[A](as: A*): Stream[A] =
-    if (as.isEmpty) empty 
+    if (as.isEmpty) empty
     else cons(as.head, apply(as.tail: _*))
 
   val ones: Stream[Int] = Stream.cons(1, ones)
-  def from(n: Int): Stream[Int] = ???
 
-  def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = ???
+  def from(n: Int): Stream[Int] = cons(n, from(n + 1))
+
+  def constant[A](a: A): Stream[A] = cons(a, constant(a))
+
+  def fibs(s: Stream[Int]): Stream[Int] = {
+    def go(i: Int, j: Int): Stream[Int] = {
+      cons(i + j, go(j, j + 1))
+    }
+    go(0, 1)
+  }
+
+  def fibsUnfold(): Stream[Int] = unfold((0, 1))(t => Some((t._1 + t._2, (t._2, t._1 + t._2))))
+
+  def fromUnfold(i: Int): Stream[Int] = unfold(i)(j => Some(j, j + 1))
+
+  def constantUnfold(i: Int): Stream[Int] = unfold(i)(j => Some(j, j))
+
+  def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] =
+    f(z) match {
+      case Some((a, s)) => cons(a, unfold(s)(f))
+      case None => empty
+    }
+
 }
--- exercises/src/test/scala/fpinscala/laziness/StreamTest.scala
+++ exercises/src/test/scala/fpinscala/laziness/StreamTest.scala
@@ -17,6 +17,11 @@ class StreamTest extends FunSuite {
   }
 
   test("Take while 1, 2, 3 not 3") {
-    assert(Stream("1", "2", "3").takeWhile(_ == "3").toList == List("1", "2"))
+    assert(Stream("1", "2", "3").takeWhile(_ != "3").toList == List("1", "2"))
   }
+
+  test("Filter 1, 2, 3 on 2") {
+    assert(Stream("1", "2", "3").filter(_ != "2").toList == List("1", "3"))
+  }
+
 }
